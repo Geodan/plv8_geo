bcda3c31aafcbb8c29528f92506ea4ad596feb60
Tom van Tilburg <tom.van.tilburg@gmail.com>
2017-11-06 20:07:11 +0100

    added togeojsonmesh

---
+++ src/d3_togeojsonmesh.sql
@@ -0,0 +1,17 @@
+
+/**
+d3_topologytomesh
+**/
+
+DROP FUNCTION IF EXISTS plv8.d3_TopologyToMesh(topology JSONB);
+CREATE FUNCTION plv8.d3_TopologyToMesh(topology JSONB)
+RETURNS SETOF JSONB
+immutable language plv8
+as $$
+	var startT = new Date();
+	//plv8.elog(NOTICE,JSON.stringify(topology));
+	var collection = topojson.mesh(topology, topology.objects.entities) 
+	var endT = new Date();
+	//plv8.elog(NOTICE,'Conversiontime: ' + (endT - startT)/1000);
+	return collection;
+$$;
